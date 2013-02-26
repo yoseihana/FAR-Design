c5e500bd6d0d13626d914ffb737766d358f3eeda
Annabelle Buffart <anna.buffart@gmail.com>
2013-02-26 19:50:53 +0100

    Accordeon Cours

--- js/script.js
+++ js/script.js
@@ -27,20 +27,50 @@
            $slider.filter(':visible').fadeOut('fast', function () {
                $nextImg.fadeIn('fast')
            });
+    };
+
+/*
+    var showYear = function(e){
+        $niveau.find('li').hide();
+        $(this).find('.year').show();
+        $(this).find('.year').on('click', showThematique);
+    };
+
+    var showThematique = function(e){
+    console.log($(this).find('ol'));
+        $(this).find('ol').show();
+        //$(this).find('.thematique').on('click', showCours);
+
+    };
+*/
+    /*var showCours = function(e){
+        console.log('ok');
+    };*/
+
+
+    var toto = function(e)
+    {
+    console.log('toto:'+e);
+    alert('toto:'+e);
     }
 
+    // Toggle OLs
+    var olToggler = function(e) {
+        $(e).children('ol').toggle();
+    }
 
     //Load de routine
     $(function () {
         $slider.not(":first").hide();
-        console.log('test');
-        $niveau.find('ol').hide();
+        $niveau.find('li').hide();
 
-        $niveau.on('click', showGrade);
+        $niveau.on('click', showYear);
 
         setInterval(switchImg, delay);
                 $('.precedent').on('click', previousImg);
                 $('.suivant').on('click', switchImg);
+
+        $('ol.tree#formation').find('li:has(ol)').each(function(i,li) { $(li).on('click', li, function(e) { olToggler(e.data); })});
     });
 
 })(jQuery);
