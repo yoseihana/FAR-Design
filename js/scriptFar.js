e12f4da3f03c1e6fa1fc62af3a9ef14e2d93ded1
Annabelle Buffart <anna.buffart@gmail.com>
2013-03-10 17:04:04 +0100

    Amélioration après réunion avec FAR et Mme Etienne

--- js/scriptFar.js
+++ js/scriptFar.js
@@ -29,48 +29,22 @@
            });
     };
 
-/*
-    var showYear = function(e){
-        $niveau.find('li').hide();
-        $(this).find('.year').show();
-        $(this).find('.year').on('click', showThematique);
-    };
-
-    var showThematique = function(e){
-    console.log($(this).find('ol'));
+    //Show Thematique
+    var showThematique = function(e) {
+        $niveau.find('.year ol').hide();
         $(this).find('ol').show();
-        //$(this).find('.thematique').on('click', showCours);
-
-    };
-*/
-    /*var showCours = function(e){
-        console.log('ok');
-    };*/
-
-
-    var toto = function(e)
-    {
-    console.log('toto:'+e);
-    alert('toto:'+e);
-    }
-
-    // Toggle OLs
-    var olToggler = function(e) {
-        $(e).children('ol').toggle();
     }
 
     //Load de routine
     $(function () {
         $slider.not(":first").hide();
-        $niveau.find('li').hide();
+        $niveau.find('.year ol').hide();
 
-        //$niveau.on('click', showYear);
+        $niveau.find('.year').on('click', showThematique);
 
         setInterval(switchImg, delay);
                 $('.precedent').on('click', previousImg);
                 $('.suivant').on('click', switchImg);
-
-        $('ol.tree#formation').find('li:has(ol)').each(function(i,li) { $(li).on('click', li, function(e) { olToggler(e.data); })});
     });
 
 })(jQuery);
