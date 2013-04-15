a36e8951d63b2fb4795c07f37fce6f0f589f61de
Annabelle Buffart <anna.buffart@gmail.com>
2013-04-15 22:17:20 +0200

    Mise à jour avec les modifications de la FAR

--- js/scriptFar.js
+++ js/scriptFar.js
@@ -2,7 +2,7 @@
     var $slider = $('.slider figure');
     var $niveau = $('.niveau');
     var $submenu = $('.submenu');
-    var delay = 5000;
+    var delay = 7000;
 
     //Slider accueil
     var switchImg = function () {
@@ -13,39 +13,49 @@
             $nextImg = $slider.first();
         }
 
-        $slider.filter(':visible').fadeOut('fast', function () {
-            $nextImg.fadeIn('fast');
+        $slider.filter(':visible').fadeOut('slow', function () {
+            $nextImg.fadeIn('slow');
         });
     };
 
-    var previousImg = function(e){
+    var previousImg = function (e) {
         var $nextImg = $slider.filter(':visible').prev();
 
-           if ($nextImg.size() == 0) {
-              $nextImg = $slider.first();
-           }
+        if ($nextImg.size() == 0) {
+            $nextImg = $slider.first();
+        }
 
-           $slider.filter(':visible').fadeOut('fast', function () {
-               $nextImg.fadeIn('fast')
-           });
+        $slider.filter(':visible').fadeOut('fast', function () {
+            $nextImg.fadeIn('fast')
+        });
     };
 
     //Show Thematique
-    var showThematique = function(e) {
-        $niveau.find('.year ol').hide();
-        $(this).find('ol').show();
-    }
+    var toggleYear = function (e) {
+        e.stopPropagation(); // Prevent parent DOM tree element events to be propagated
+        $(e.target).parents('.year').find('ol').toggle();
+    };
+
+    //Show documents
+    var toggleThematique = function (e) {
+        e.stopPropagation(); // Prevent parent DOM tree element events to be propagated
+        $(e.target).parents('.thematique').find('ul').toggle();
+
+    };
 
     //Load de routine
     $(function () {
         $slider.not(":first").hide();
-        $niveau.find('.year ol').hide();
 
-        $niveau.find('.year').on('click', showThematique);
+        $niveau.find('.year ol').hide();
+        $niveau.find('.thematique ul').hide();
+
+        $niveau.find('.year').click(toggleYear);
+        $niveau.find('.thematique').click(toggleThematique);
 
         setInterval(switchImg, delay);
-                $('.precedent').on('click', previousImg);
-                $('.suivant').on('click', switchImg);
+        $('.precedent').on('click', previousImg);
+        $('.suivant').on('click', switchImg);
     });
 
 })(jQuery);
